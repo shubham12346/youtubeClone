95dcea3d83f2f18df21a0bfebc2bbb9483d011c4
shubham agrahari <aakashagrahari26@gmail.com>
2024-09-01 00:35:55 +0530

    Added file

--- src/router/router.js
+++ src/router/router.js
@@ -3,19 +3,22 @@ import VideoIndex from "../component/VideoIndex";
 import VideoPlay from "../videoplayer/VideoPlay";
 import Base from "../component/Base";
 
-export const router = createBrowserRouter([
-  {
-    path: "",
-    element: <Base />,
-    children: [
-      {
-        path: "",
-        element: <VideoIndex />,
-      },
-      {
-        path: "/video/:id",
-        element: <VideoPlay />,
-      },
-    ],
-  },
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      path: "",
+      element: <Base />,
+      children: [
+        {
+          path: "",
+          element: <VideoIndex />,
+        },
+        {
+          path: "/video/:id",
+          element: <VideoPlay />,
+        },
+      ],
+    },
+  ],
+  { basename: "/youtubeClone" }
+);
